7155f5da47bb50baa683de91ee8e3a0c76fd872e
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-06-09 15:40:16 -0600

    Formularios

---
+++ src/main/java/com/mycompany/llvgcrud03javaswing/LLVGCRUD03JAVASWING.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.llvgcrud03javaswing;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class LLVGCRUD03JAVASWING {
+
+    public static void main(String[] args) {
+       FrmInicio frmInicio= new FrmInicio();
+       frmInicio.setVisible(true);
+    }
+}
